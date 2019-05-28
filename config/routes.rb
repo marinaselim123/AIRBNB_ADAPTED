e2c870c4c1271b03e00835f0213bed17e2636a9f
Anthony Faoro <mamaaiiiing@gmail.com>
2019-05-28 12:51:44 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
-  get 'accommodations/index'
-  get 'accommodations/create'
-  get 'accommodations/new'
-  get 'accommodations/show'
-  get 'accommodations/my_reservation'
-  get 'accommodations/accepted'
-  get 'accommodations/declined'
-  get 'reservations/index'
-  get 'reservations/create'
-  get 'reservations/new'
-  get 'reservations/show'
-  get 'reservations/my_accommodation'
-  devise_for :users
   root to: 'pages#home'
+  resources :accommodations
+  # get "/accommodations", to: "accommodations#index"
+  # post "/accommodations", to: "accommodations#create"
+  # get "/accommodation/new", to: "accommodations#new"
+  # get "/accommodations/:id", to: "accommodations#show"
+  # get "/accommodations/reservations", to: "accommodations#reservations"
+  # get "/accommodations/accepted", to: "accommodations#accepted"
+  # get "/accommodations/declined", to: "accommodations#declined"
+  # get 'reservations/index'
+  # get 'reservations/create'
+  # get 'reservations/new'
+  # get 'reservations/show'
+  # get 'reservations/my_accommodation'
+  devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
