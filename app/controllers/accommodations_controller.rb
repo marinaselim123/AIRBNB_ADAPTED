02164428a05e02cf15629c995d868603818a5a0e
Anthony Faoro <mamaaiiiing@gmail.com>
2019-05-28 14:57:07 +0200

    accommodation controller

--- app/controllers/accommodations_controller.rb
+++ app/controllers/accommodations_controller.rb
@@ -1,14 +1,23 @@
 class AccommodationsController < ApplicationController
   def index
+    @accommodations = Accommodation.all
   end
 
   def create
+    @accommodation = Accommodation.new(accommodation_params)
+    if @accommodation.save
+      redirect_to accommodation_path
+    else
+      render :new
+    end
   end
 
   def new
+    @accommodation = Accommodation.new
   end
 
   def show
+    @accommodation = Cocktail.find(params[:id])
   end
 
   def my_accommodation
@@ -18,5 +27,11 @@ class AccommodationsController < ApplicationController
   end
 
   def declined
+  end
+
+  private
+
+  def accommodation_params
+    params.require(:cocktail).permit(:type, :guest_number, :rooms_number, :beds_number, :toilet_number, :address, :price_per_night, :image)
   end
 end
