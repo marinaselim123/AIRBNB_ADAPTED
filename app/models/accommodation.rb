b04a1adfc49d7593d4844de212da54ca20eea27e
marinaselim123 <marina.selim@kedgebs.com>
2019-05-28 15:41:25 +0200

    correction routes

--- app/models/accommodation.rb
+++ app/models/accommodation.rb
@@ -2,6 +2,5 @@ class Accommodation < ApplicationRecord
   belongs_to :user
   has_many :reservations
   has_many :users, through: :reservations
-  validates :type, :guest_number, :rooms_number, :beds_number, :toilet_number, :address, :price_per_night, :image,
-  presence: true
+  validates :type, :guest_number, :rooms_number, :beds_number, :toilet_number, :address, :price_per_night, presence: true
 end
