6d6da79395b0eddd1c0efcc5d9bbe47a3c20a8b0
Alice Houzelot <alicehouzelot@hotmail.fr>
2019-05-31 16:45:43 +0200

    updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,26 +12,26 @@ User.destroy_all
 
 puts "Creating users..."
 
-toto = User.create!(
-  first_name: 'Toto',
-  last_name: 'Titi',
+jean = User.create!(
+  first_name: 'Jean',
+  last_name: 'Dupont',
   address: '20 rue de Rome, Marseille',
-  email: 'toto@gmail.com',
+  email: 'jean@gmail.com',
   password: 'password'
 )
 
-User.create!(
-  first_name: 'Titi',
-  last_name: 'Toto',
+marina = User.create!(
+  first_name: 'Marina',
+  last_name: 'Selim',
   address: '22 rue de Rome, Marseille',
-  email: 'titi@gmail.com',
+  email: 'marina@gmail.com',
   password: 'password'
 )
 
 puts "Creating accommodations..."
 
 Accommodation.create!(
-    user: toto,
+    user: jean,
     guest_number: rand(1..4),
     rooms_number: rand(1..5),
     beds_number: rand(1..4),
@@ -39,12 +39,12 @@ Accommodation.create!(
     address: "167 rue Paradis, Marseille",
     price_per_night: rand(20..100),
     accommodation_type: "Loft with panorama",
-    remote_photo_url: "https://odis.homeaway.com/odis/listing/fcf8f760-1bb2-4862-b678-bdc79ba1202f.c10.jpg"
+    remote_photo_url: "http://www.freshdesignpedia.com/wp-content/uploads/60-inspiring-balcony-ideas-so-are-you-a-fantastic-balcony-design/designer-balcony-ideas-summer-house-with-sea-view-gorgeous.jpg"
   )
 
 
   Accommodation.create!(
-    user: toto,
+    user: jean,
     guest_number: rand(1..4),
     rooms_number: rand(1..5),
     beds_number: rand(1..4),
@@ -56,12 +56,12 @@ Accommodation.create!(
   )
 
    Accommodation.create!(
-    user: toto,
+    user: jean,
     guest_number: rand(1..4),
     rooms_number: rand(1..5),
     beds_number: rand(1..4),
     toilet_number: rand(1..4),
-    address: "12 allée marcel leclerc, Marseille",
+    address: "Stade Vélodrome, Marseille",
     price_per_night: rand(20..100),
     accommodation_type: "Studio behind the stadium",
     remote_photo_url: "https://lesjours.fr/ressources/image/ep2-velodrome-img-header.jpeg"
@@ -69,30 +69,41 @@ Accommodation.create!(
 
 
    Accommodation.create!(
-    user: toto,
+    user: marina,
     guest_number: rand(1..4),
     rooms_number: rand(1..5),
     beds_number: rand(1..4),
     toilet_number: rand(1..4),
-    address: "25 boulevard garoutte, Marseille",
+    address: "Vieux Port, Marseille",
     price_per_night: rand(20..100),
     accommodation_type: "Flat at Le Vieux Port",
-    remote_photo_url: "https://www.artcom.fr/img/projets/normal/appart2.jpg"
+    remote_photo_url: "https://www.ahstatic.com/photos/0542_ho_00_p_1024x768.jpg"
   )
 
 
 Accommodation.create!(
-    user: toto,
+    user: marina,
     guest_number: rand(1..4),
     rooms_number: rand(1..5),
     beds_number: rand(1..4),
     toilet_number: rand(1..4),
     address: "Tour Eiffel, Paris",
     price_per_night: rand(20..100),
-    accommodation_type: "Cozy apartment in the center",
-    remote_photo_url: "https://madeinmarseille.net/actualites-marseille/fontaine-place-castellane-tourisme-marseille.jpg"
+    accommodation_type: "Appartment with a view",
+    remote_photo_url: "http://www.book-a-flat.com/magazine/wp-content/uploads/2015/10/terrasse-vue-tour-eiffel-paris.jpg"
     )
 
+Accommodation.create!(
+    user: marina,
+    guest_number: rand(1..4),
+    rooms_number: rand(1..5),
+    beds_number: rand(1..4),
+    toilet_number: rand(1..4),
+    address: "Lisbonne",
+    price_per_night: rand(20..100),
+    accommodation_type: "Studio in the center",
+    remote_photo_url: "https://t-ec.bstatic.com/images/hotel/max1024x768/351/35166590.jpg"
+    )
 
 # Reservation.create!(arrival_date: DateTime.parse("08/09/2019"), departure_date: DateTime.parse("23/09/2019"), user: User.last, guest_number: 3, total_price: 45, accommodation: Accommodation.last, status: "pending" )
 
